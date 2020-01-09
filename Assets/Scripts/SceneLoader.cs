5dd9481e4b119a08b7115cec2f24e6151fb7cef0
Dellintel98 <asolic00@fesb.hr>
2020-01-09 02:04:56 +0100

    Modify scenes and add sprites

--- Assets/Scripts/SceneLoader.cs
+++ Assets/Scripts/SceneLoader.cs
@@ -1,18 +1,39 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class SceneLoader : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void LoadDesiredScene(string desiredScene) 
     {
-        
+        //int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        int desiredSceneIndex;
+
+        switch (desiredScene) 
+        {
+            case "ChessMenu":
+                desiredSceneIndex = 1;
+                break;
+            case "ChessGame":
+                desiredSceneIndex = 2;
+                break;
+            case "GoMenu":
+                desiredSceneIndex = 3;
+                break;
+            case "GoGame":
+                desiredSceneIndex = 4;
+                break;
+            default:
+                desiredSceneIndex = 0;
+                break;
+        }
+
+        SceneManager.LoadScene(desiredSceneIndex);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void QuitGame()
     {
-        
+        Application.Quit();
     }
 }
